dd2ce5bd5950f615b12dd403ef6b771d6d3599c8
darcanj0 <daniel.thomas.aarcanjo@gmail.com>
2023-04-04 21:42:41 -0300

    refactor: separate questionnaira and result in widgets

---
+++ lib/questionnaire.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:questions_app/answer_button.dart';
+import 'package:questions_app/question.dart';
+
+class Questionnaire extends StatelessWidget {
+  final int selectedQuestion;
+  final List<Map<String, Object>> questions;
+  final void Function() onAnswer;
+
+  const Questionnaire({
+    super.key,
+    required this.questions,
+    required this.onAnswer,
+    required this.selectedQuestion,
+  });
+
+  bool get _hasSelectedQuestion {
+    return selectedQuestion < questions.length;
+  }
+
+  List<Map<String, Object>> getAnswers() {
+    return _hasSelectedQuestion
+        ? questions[selectedQuestion].cast()['answers']
+            as List<Map<String, Object>>
+        : [];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Question(questions[selectedQuestion]['question'].toString()),
+        ...getAnswers()
+            .map((answer) => AnswerButton(answer['text'] as String, onAnswer))
+            .toList()
+      ],
+    );
+  }
+}
